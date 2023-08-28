36fa024027f2bd65430fbd0ce639242f058a016a
Matheus Pestana <125031562+matheus-pestana@users.noreply.github.com>
2023-08-28 15:52:48 -0300

    Add files via upload
    Commit inicial após alterações de responsividade e criação de novo repositório.

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// LOGIN
+
+function mostrarsenha() {
+
+    if (document.getElementById("senha").type == "password") {
+        document.getElementById("senha").type = "text";
+        document.getElementById("Eye_Simbol").src = "img/Olho2.png";
+    } else {
+        document.getElementById("senha").type = "password";
+        document.getElementById("Eye_Simbol").src = "img/Olho1.png";
+    }
+}
